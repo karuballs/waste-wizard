f451fdc9dd13f3c75befed383a00d7434f4d8d24
gradhav001 <jesse.richard@strathmore.edu>
2023-11-14 23:52:33 +0300

    some refinements

--- driverLogin.js
+++ driverLogin.js
@@ -6,6 +6,9 @@ let cpassword = document.querySelector('.cpassword');
 let spaceCheck = /\s/g;
 let lowercaseCheck = /\[a-z]+gm/;
 let uppercase = /\[A-Z]+gm/;
+let licensePlateInput = document.querySelector('#licensePlate');
+let licensePlateError = document.querySelector('.error-1');
+let licensePlateCheck = /^[A-Z]{3}[0-9]{3}[A-Z]$/;
 
 console.log(passError);
 
@@ -23,20 +26,33 @@ form.addEventListener('submit', function (e){
 });
 
 password.addEventListener('input', function (){
-    if(numCheck.test(password.value) == false){
+    if(password.value == ""){
+        passError.innerHTML = " ";
+    }
+    else if(numCheck.test(password.value) == false){
        passError.innerHTML = "you should have a number in your password";
-    }else if(lowercaseCheck.test(password.value) == false){
-        passError.innerHTML = "you should have a lowercasein your password";
+    }
+    else if(lowercaseCheck.test(password.value) == false){
+        passError.innerHTML = "you should have a lowercase in your password";
     }
     else if(uppercase.test(password.value) == false){
         passError.innerHTML = "You do not have an uppercase in your password";
     }
     else if(numCheck.test(password.value) == true && lowercaseCheck.test(password.value) == true && uppercase.test(password.value) == true){
         passError.innerHTML = " ";
-    }else if(numCheck.test(password.value) == false && lowercaseCheck.test(password.value) == false && uppercase.test(password.value) == false){
-        passError.innerHTML = " ";
     }
-    
-    
-
 })
+
+licensePlateInput.addEventListener('input', function (){
+    if(licensePlateInput.value == ""){
+        licensePlateError.innerHTML = " ";
+    }
+    else if(licensePlateCheck.test(licensePlateInput.value) == false){
+       licensePlateError.innerHTML = "Invalid license plate number. The format should be 3 capital letters followed by three numbers and finally a capital letter at the end.";
+    }
+    else{
+        licensePlateError.innerHTML = " ";
+    }
+})
+
+
